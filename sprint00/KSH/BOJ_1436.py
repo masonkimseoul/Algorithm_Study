a746e0396c2127d3bdd83495401f884ad83df7ec
sunghunkim <sunghun1998@naver.com>
2022-05-17 19:54:23 +0900

    BOJ_1436.py

---
+++ sprint00/KSH/BOJ_1436.py
@@ -0,0 +1,12 @@
+if __name__ == "__main__":
+    
+    num = int(input())
+
+    n = 666
+    count = 0
+    while (count < num):
+        if "666" in str(n):
+            count += 1
+        n += 1
+    
+    print(n - 1)
