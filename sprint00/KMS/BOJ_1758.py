80d84c8db2c04d5edf81ddaa90fefb5a1654cf3b
MINSU <kmsu444@naver.com>
2022-05-22 04:26:05 +0900

    abc

---
+++ sprint00/KMS/BOJ_1758.py
@@ -0,0 +1,17 @@
+from itertools import *
+L,C = map(int, input().split())
+list1 = []
+list1 = input().split()
+list1 = sorted(list1)
+for a in combinations(list1,L):
+    consonant = 0
+    Vowel =0
+    for i in a:
+        if(i == 'a' or i == 'e' or i =='i' or i =='o' or i =='u'):
+            consonant = consonant + 1
+        else:
+            Vowel = Vowel + 1
+    
+    if(consonant >=1 and Vowel >= 2):
+        result = ''.join(s for s in a)
+        print(result)
