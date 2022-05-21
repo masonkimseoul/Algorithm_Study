2c4fae5762095cea6f8f9ad510c277d6bfa43ad1
MINSU <kmsu444@naver.com>
2022-05-22 03:44:41 +0900

    22-05-22

---
+++ sprint00/KMS/BOJ_1182.py
@@ -0,0 +1,18 @@
+from itertools import *
+N,S = map(int, input().split())
+list1 = map(int, input().split())
+list1 = list(list1)
+cnt = 0
+
+for i in range(1,N+1):
+    for a in combinations(list1, i):
+        if(sum(a) == S):
+            cnt = cnt+1
+print(cnt)
+    
+
+
+
+
+
+    
