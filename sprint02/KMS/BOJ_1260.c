2285276a2419c291ae1ac6ec55fbada06b2a7d86
minsu <kmsu444@naver.com>
2022-06-26 17:37:55 +0900

    11

--- sprint02/KMS/BOJ_1260.c
+++ sprint02/KMS/BOJ_1260.c
@@ -3,87 +3,57 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-int visit[100] = {0};
-int visit1[100] = { 0 };
+#define MAX_VERTICES 1001
+int DFS_V[MAX_VERTICES] = { 0, }; //DFS를 실행하면서 방문한 정점을 표시하기 위한 배열
+int BFS_V[MAX_VERTICES] = { 0, }; //BFS를 실행하면서 방문한 정점을 표시하기 위한 
+int graph[MAX_VERTICES][MAX_VERTICES] = { 0, };
+int queue[MAX_VERTICES];
+void dfs(int v, int vertices);
+void bfs(int v, int vertices);
 
+int main() {
+    int vertices, edges, vertex, i, j;
+    scanf("%d %d %d", &vertices, &edges, &vertex);
 
-void dfs(int G[100][100], int v, int vsize) {
-	visit[v] = 1;
-	printf(" %d", v+1);
-	/*
-	printf("visit : %d\n", v);
-	for (int i = 0; i < vsize; i++) {
-		for (int j = 0; j < vsize; j++) {
-			printf(" %d", G[i][j]);
-		}
-		printf("\n");
-	}
-	*/
-	for (int i = 0; i < vsize; i++) {
-		if (G[v][i] == 1 && visit[i] == 0) {
-			dfs(G, i, vsize);
-		}
-	}	
+    while (edges--) {
+        scanf("%d %d", &i, &j);
+        graph[i][j] = 1;
+        graph[j][i] = 1;
+    }
+
+    dfs(vertex, vertices);
+    printf("\n");
+    bfs(vertex, vertices);
+
+    return 0;
 }
 
-typedef struct Queue {
-	int arr[100];
-	int front;
-	int rear;
-}Queue;
-int dequeue(Queue *q) {
-	if (q->front==q->rear) {
-		return 0;
-	}
-	int data;
-	data = q->arr[q->front];
-	q->front = (q->front +1) % 100;
-	return data;
+void dfs(int v, int vertices) {
+    int w;
+    DFS_V[v] = 1;
+    printf("%d ", v);
+    for (w = 1; w <= vertices; w++) {
+        if (graph[v][w] == 1 && DFS_V[w] == 0) {
+            dfs(w, vertices);
+        }
+    }
 }
-void enqueue(Queue *q,int data) {
-	if ((q->rear+1) % 100 == q->front) {
-		return;
-	}
-	q->arr[q->rear] = data;
-	q->rear = (q->rear + 1) % 100;
-}
-Queue q;
-void bfs(int G[100][100], int v, int vsize) {
-	int t;
-	//데이터 추가
-	enqueue(&q, v);
-	visit1[v] = 1;
-	while (q.front != q.rear) {
-		t = dequeue(&q);
-		for (int i = 0; i < vsize; i++) {
-			if (G[t][i] = 1 && visit1[i] == 0) {
-				visit1[i] = 1;
-				enqueue(&q, i);
-			}
-		}
-		printf(" %d", t+1);
-	}
-}
-int main()
-{
-	int V, E, start;
-	scanf("%d %d %d", &V, &E, &start);
-	int G[100][100] ={0};
-	int a, b;
-	for (int i = 0; i < E; i++) {
-		scanf("%d %d", &a, &b);
-		G[a-1][b-1] = 1;
-		G[b - 1][a - 1] = 1;
-	}
-	dfs(G, start-1, V);
-	
-	q.front = 0;
-	q.rear = 0;
-	printf("\n");
-	bfs(G, start - 1, V);
 
-
-
-
-	return 0;
+void bfs(int v, int vertices) {
+    int w;
+    int front, rear, pop;
+    front = rear = 0;
+    printf("%d ", v);
+    BFS_V[v] = 1;
+    queue[0] = v; rear++;
+    while (front < rear) {
+        pop = queue[front]; front++;
+        for (w = 1; w <= vertices; w++) {
+            if (graph[pop][w] == 1 && BFS_V[w] == 0) {
+                printf("%d ", w);
+                queue[rear] = w; rear++;
+                BFS_V[w] = 1;
+            }
+        }
+    }
 }
