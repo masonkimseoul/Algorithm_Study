1e59058924bfb8fde2d34f6044f03d32699947a3
sunghunkim <sunghun1998@naver.com>
2022-07-03 18:43:40 +0900

    add test case link in PGS_43163.py

--- sprint02/KSH/PGS_43163.py
+++ sprint02/KSH/PGS_43163.py
@@ -51,3 +51,12 @@ def solution(begin, target, words):
     
     return answer
 
+# test case 공유
+# 링크: https://programmers.co.kr/questions/15939
+
+# print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]), 4)
+# print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log"]), 0)
+# print(solution("hit", "hot", ["hot", "dot", "dog", "lot", "log"]), 1)
+# print(solution("1234567000", "1234567899", [
+#       "1234567800", "1234567890", "1234567899"]), 3)
+# print(solution("hit", "cog", ["cog", "log", "lot", "dog", "hot"]), 4)
