093a95e3d0cb4decc32392e3b0ca54ad5b6a97ae
Dahyeon Kye <rpekgus@gmail.com>
2022-08-05 01:38:33 +0900

    [PGS] 86491 / 최소직사각형

---
+++ sprint05/KDH/01/PGS_86491.py
@@ -0,0 +1,11 @@
+def solution(sizes):
+    answer = 0
+    max1, max2 = -1, -1
+
+    for size in sizes:
+        size.sort()
+
+        max1 = max(max1, size[1])
+        max2 = max(max2, size[0])
+
+    return max1*max2
