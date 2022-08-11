b2e53fc8a6af673c0e5265b1d383fb088d691be5
Dahyeon Kye <rpekgus@gmail.com>
2022-08-11 17:04:16 +0900

    [PGS] 42586 / 기능개발

--- sprint05/KDH/01/PGS_42586.py
+++ sprint05/KDH/01/PGS_42586.py
@@ -3,7 +3,6 @@ import math
 
 def solution(progresses, speeds):
     answer = []
-    days = []
     for p, s in zip(progresses, speeds):
         if not answer or release < math.ceil((100-p)/s):
             release = math.ceil((100-p)/s)
