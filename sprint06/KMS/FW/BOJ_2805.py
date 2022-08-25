c317dcc8f6fe8a6a01209f6dc4d7cee704190dcb
민수 <wan@minsuui-MacBookPro.local>
2022-08-25 12:59:39 +0900

    1

---
+++ sprint06/KMS/FW/BOJ_2805.py
@@ -0,0 +1,21 @@
+import numbers
+import sys
+N,M = map(int, input().split())
+L = list(map(int, sys.stdin.readline().split()))
+start, end = 0, max(L)
+print(start, end)
+result = 0
+while start <= end:
+    take = 0
+    mid = (end + start) //2
+    for i in L:
+        if i > mid:
+            take+= i - mid
+    if take >= M:
+        start = mid +1
+        result = mid
+    else:
+        end = mid -1
+print(result)
+# 4 7
+# 20 15 10 17
--- sprint06/KMS/FW/BOJ_4386.py
+++ sprint06/KMS/FW/BOJ_4386.py
@@ -1,4 +1,4 @@
-from dis import dis
+from collections import deque
 import math
 def distance(a,b,c,d):
     return round(math.sqrt((a-c)*(a-c)+(b-d)*(b-d)),2)
@@ -12,9 +12,29 @@ for i in range(N):
     X = int(float(x) * 100)
     Y = int(float(y) * 100)
     L.append([X,Y])
-line = []
-print(L)
-for i in range(len(L)-1):
-    for j in range(i+1,len(L)):
-        line.append(distance(L[i][0],L[i][1],L[j][0],L[j][1]))
-print(line)
+matrix = [[0 for _ in range(N)] for _ in range(N)]
+for i in range(len(L)):
+    for j in range(len(L)):
+        if i==j:
+            continue
+        else:
+            x,y = L[i]
+            xx,yy = L[j]
+            matrix[i][j] = distance(x,y,xx,yy)
+visit =[0 for _ in range(N)]
+S = 0
+for i in range(N):
+    mind = 0
+    save_index = -1
+    
+    for jinx,j in enumerate(matrix[i]):
+        if j == 0:
+            continue
+        if mind ==0:
+            mind = j
+        else:
+            if mind > j:
+                mind = j
+                save_index = jinx
+    S += mind
+print(S)
