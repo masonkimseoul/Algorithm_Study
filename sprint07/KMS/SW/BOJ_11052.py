ca813c76083bddc3f016108d06031da4b1d287f5
민수 <wan@minsuui-MacBookPro.local>
2022-09-07 20:27:50 +0900

    11

---
+++ sprint07/KMS/SW/BOJ_11052.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+L = [0] + list(map(int, input().split()))
+dp = [0] *(N+1)
+for i in range(1,N+1):
+    for j in range(1,i+1):
+        dp[i] = max(dp[i-j] + L[j],dp[i])
+print(dp)
+print(dp[N])
