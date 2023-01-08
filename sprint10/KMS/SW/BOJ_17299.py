d8aec295ff4d0c7cb12939ae48bc65761ab88de1
kmsu44 <kmsu444@naver.com>
2023-01-08 15:36:28 +0900

    오등 큰수 ㅂ q

---
+++ sprint10/KMS/SW/BOJ_17299.py
@@ -0,0 +1,16 @@
+import sys
+N = int(input())
+arr = list(map(int,sys.stdin.readline().split()))
+D = [0] * 10000000
+
+for i in arr:
+  D[i] +=1
+result = [-1] * N
+stack =[]
+stack.append(0)
+for i in range(1,N):
+  while stack and D[arr[stack[-1]]] < D[arr[i]]:
+    result[stack.pop()] = arr[i]
+  stack.append(i)
+print(*result)
+    
