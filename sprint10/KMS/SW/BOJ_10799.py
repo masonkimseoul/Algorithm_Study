fa60502349c0e55e574cbc6e034260d117c7f4bb
kmsu44 <kmsu444@naver.com>
2023-01-10 21:38:28 +0900

    plus

--- sprint10/KMS/SW/BOJ_10799.py
+++ sprint10/KMS/SW/BOJ_10799.py
@@ -7,25 +7,25 @@ stick = []
 result = []
 i = 0
 while True:
-  if n[i] == '(' and n[i+1] == ')':
-    lazer.append(i)
-    i+=2
-    continue
-  elif n[i] == '(':
-    stick.append(i)
-  elif n[i] == ')':
-    t = stick.pop()
-    result.append((t,i))
-  i+=1
-  if i == size:
-    break
+    if n[i] == '(' and n[i+1] == ')':
+        lazer.append(i)
+        i += 2
+        continue
+    elif n[i] == '(':
+        stick.append(i)
+    elif n[i] == ')':
+        t = stick.pop()
+        result.append((t, i))
+    i += 1
+    if i == size:
+        break
 
 count = 0
 
 for i in result:
-  start, end = i
-  count +=1
-  for j in lazer:
-    if start <= j <= end:
-      count+=1
+    start, end = i
+    count += 1
+    for j in lazer:
+        if start <= j <= end:
+            count += 1
 print(count)
