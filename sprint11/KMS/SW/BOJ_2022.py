97f8d8331cad8a19a0867db4fdee61b01b12de89
kmsu44 <kmsu444@naver.com>
2023-01-26 00:32:37 +0900

    BOJ_2022

---
+++ sprint11/KMS/SW/BOJ_2022.py
@@ -0,0 +1,17 @@
+import math
+x, y, c = map(float, input().split())
+
+r = min(x, y)
+l = 0
+
+while abs(l-r) >= 0.001:
+    d = (l+r) / 2
+    h1 = math.sqrt(pow(x, 2) - pow(d, 2))
+    h2 = math.sqrt(pow(y, 2) - pow(d, 2))
+    h = h1 * h2 / (h1 + h2)
+    if h <= c:
+        r = d
+    else:
+        l = d
+print(l, r, d)
+print(round(d, 3))
